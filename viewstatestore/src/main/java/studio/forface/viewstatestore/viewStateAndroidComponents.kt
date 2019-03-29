0b394ab88df779c8c2eeb230b90b29171dc91c37
4face-studi0 <37957092+4face-studi0@users.noreply.github.com>
2019-03-29 14:36:27 +0100

    Fixed `ViewStateActivity` and `ViewStateFragment` for observe to `LockedViewStateStore`

--- viewstatestore/src/main/java/studio/forface/viewstatestore/viewStateAndroidComponents.kt
+++ viewstatestore/src/main/java/studio/forface/viewstatestore/viewStateAndroidComponents.kt
@@ -8,23 +8,23 @@ import androidx.lifecycle.LifecycleOwner
  * @author Davide Giuseppe Farella.
  * An interface containing `Lifecycle`-driven observe extensions functions for `Activity`
  *
- * Implements this interface on your `Activity` for be able to call [AbsViewStateStore.observe] and
- * [AbsViewStateStore.observeData] without passing the [LifecycleOwner] explicitly.
+ * Implements this interface on your `Activity` for be able to call [LockedViewStateStore.observe] and
+ * [LockedViewStateStore.observeData] without passing the [LifecycleOwner] explicitly.
  * I.e. `myViewStateStore.observeData( ::updateUi )` it's a valid function that will observe the data respecting the
  * `Activity`s `Lifecycle`.
  */
 interface ViewStateActivity: LifecycleOwner {
     /**
-     * Call [AbsViewStateStore.observe] within an `Activity` as [LifecycleOwner]
+     * Call [LockedViewStateStore.observe] within an `Activity` as [LifecycleOwner]
      * This function can't be inlined since it's inside an interface.
      */
-    fun <V> AbsViewStateStore<V>.observe( block: ViewStateObserver<V>.() -> Unit  ) =
+    fun <V> LockedViewStateStore<V>.observe( block: ViewStateObserver<V>.() -> Unit  ) =
         observe(this@ViewStateActivity, block )
     /**
-     * Call [AbsViewStateStore.observeData] within an `Activity` as [LifecycleOwner]
+     * Call [LockedViewStateStore.observeData] within an `Activity` as [LifecycleOwner]
      * This function can't be inlined since it's inside an interface.
      */
-    fun <V> AbsViewStateStore<V>.observeData( block: (V) -> Unit  ) =
+    fun <V> LockedViewStateStore<V>.observeData( block: (V) -> Unit  ) =
         observeData(this@ViewStateActivity, block )
 }
 
@@ -32,8 +32,8 @@ interface ViewStateActivity: LifecycleOwner {
  * @author Davide Giuseppe Farella.
  * An interface containing `Lifecycle`-driven observe extensions functions for `Fragment`
  *
- * Implements this interface on your `Fragment` for be able to call [AbsViewStateStore.observe] and
- * [AbsViewStateStore.observeData] without passing the [LifecycleOwner] explicitly.
+ * Implements this interface on your `Fragment` for be able to call [LockedViewStateStore.observe] and
+ * [LockedViewStateStore.observeData] without passing the [LifecycleOwner] explicitly.
  * I.e. `myViewStateStore.observeData( ::updateUi )` it's a valid function that will observe the data respecting the
  * `Fragment`s `Lifecycle`.
  */
@@ -46,15 +46,15 @@ interface ViewStateFragment {
     fun getViewLifecycleOwner(): LifecycleOwner
 
     /**
-     * Call [AbsViewStateStore.observe] within a [getViewLifecycleOwner] as [LifecycleOwner]
+     * Call [LockedViewStateStore.observe] within a [getViewLifecycleOwner] as [LifecycleOwner]
      * This function can't be inlined since it's inside an interface.
      */
-    fun <V> AbsViewStateStore<V>.observe( block: ViewStateObserver<V>.() -> Unit  ) =
+    fun <V> LockedViewStateStore<V>.observe( block: ViewStateObserver<V>.() -> Unit  ) =
         observe( getViewLifecycleOwner(), block )
     /**
-     * Call [AbsViewStateStore.observeData] within a [getViewLifecycleOwner] as [LifecycleOwner]
+     * Call [LockedViewStateStore.observeData] within a [getViewLifecycleOwner] as [LifecycleOwner]
      * This function can't be inlined since it's inside an interface.
      */
-    fun <V> AbsViewStateStore<V>.observeData( block: (V) -> Unit  ) =
+    fun <V> LockedViewStateStore<V>.observeData( block: (V) -> Unit  ) =
         observeData( getViewLifecycleOwner(), block )
 }
