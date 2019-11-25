fb4410f93349577731a88f23d061e4400caffbfc
4face-studi0 <4face.studio@gmail.com>
2019-11-25 19:17:55 +0800

    Better `FromLiveDataTest`

--- viewstatestore/src/test/kotlin/studio/forface/viewstatestore/FromLiveDataTest.kt
+++ viewstatestore/src/test/kotlin/studio/forface/viewstatestore/FromLiveDataTest.kt
@@ -3,6 +3,7 @@ package studio.forface.viewstatestore
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.switchMap
+import io.mockk.Ordering
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Rule
@@ -88,7 +89,7 @@ internal class FromLiveDataTest {
     fun `ViewStateStore emits correctly from LiveData_switchMap`() {
         val s1 = MutableLiveData<Int>()
         val s2 = MutableLiveData<Int>()
-        val m = ViewStateStore.from(s1.switchMap { v1 -> s2.map { v2 -> v1 * v2 } })
+        val m = ViewStateStore.from(s1.switchMap { v1 -> s2.map { v2 -> v1 * v2 } }).lock
 
         // Start Observing
         val observer = mockk<(Int) -> Unit>(relaxed = true)
@@ -101,21 +102,20 @@ internal class FromLiveDataTest {
         verify(exactly = 0) { observer(any()) }
         s2.value = 1
         verify(exactly = 1) { observer(any()) }
-        verify(exactly = 1) { observer(1) }
 
-        // Verify emit on s1
-        s1.value = 2
-        verify(exactly = 2) { observer(any()) }
-        verify(exactly = 1) { observer(2) }
+        // Emit on s1, then twice on s1 and then on s1 again
+        s1.value = 3 // s2 = 1 - total: 3
+        s2.value = 2 // s1 = 3 - total: 6
+        s2.value = 4 // s1 = 3 - total: 12
+        s1.value = 4 // s2 = 4 - total: 16
 
-        // Verify emit on s2
-        s2.value = 2
-        verify(exactly = 3) { observer(any()) }
-        verify(exactly = 1) { observer(4) }
-
-        // Re-verify emit on s1
-        s1.value = 3
-        verify(exactly = 4) { observer(any()) }
-        verify(exactly = 1) { observer(6) }
+        // Verify exactly, ordered, calls on the observer
+        verify(Ordering.SEQUENCE) {
+            observer(1)
+            observer(3)
+            observer(6)
+            observer(12)
+            observer(16)
+        }
     }
 }
