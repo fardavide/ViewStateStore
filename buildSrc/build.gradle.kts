5d4552e2c9b6d3206110122ca25034cc29ea8cfe
4face-studi0 <4face.studio@gmail.com>
2020-03-01 10:36:33 +0100

    Updated Gradle and EasyGradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     val android =       "3.6.1"         // Updated: Feb 28, 2020
-    val easyGradle =    "1.1.0"         // Updated: Oct 19, 2019
+    val easyGradle =    "1.2.3-beta-3"  // Updated: Jan 04, 2020
 
     implementation("com.android.tools.build:gradle:$android")
     implementation("studio.forface.easygradle:dsl-android:$easyGradle")
